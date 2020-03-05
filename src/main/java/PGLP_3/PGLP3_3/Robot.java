b687c36da107bfe80a2a1aff9c0aae75526c1f7e
KI Serge Laobien <sergelaobienk@gmail.com>
2020-03-05 21:56:27 +0000

    modif robot

--- src/main/java/PGLP_3/PGLP3_3/Robot.java
+++ src/main/java/PGLP_3/PGLP3_3/Robot.java
@@ -3,3 +3,14 @@ package PGLP_3.PGLP3_3;
 public class Robot {
 
 }
+public class Robot {
+	public int position;
+	public Robot(int position) {
+		this.position=position;
+	}
+	public void avance()
+	{
+		this.position+=10;
+		
+	}
+}
