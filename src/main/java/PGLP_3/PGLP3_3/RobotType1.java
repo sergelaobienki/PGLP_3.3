43f204dfe0aa8116db080e96f4fde8754ea2655f
KI Serge Laobien <sergelaobienk@gmail.com>
2020-03-06 08:55:07 +0000

    modif1 robottyp1

--- src/main/java/PGLP_3/PGLP3_3/RobotType1.java
+++ src/main/java/PGLP_3/PGLP3_3/RobotType1.java
@@ -1,11 +1,12 @@
 package PGLP_3.PGLP3_3;
 
-public class RobotType1 {
+public class RobotType1 extends Robot{
+
 	public RobotType1(int position) {
 		super(position);
 	}
-	public void avance()
-	{
-		super.position+=10;
-	}
+public void avance()
+{
+	super.position+=10;
+}
 }
